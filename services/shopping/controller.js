7e77213550ba543da098b3fe4f8d01d688b357b1
Eudes Inacio <eudes.inacio@tistech.co.ao>
2021-01-24 16:48:27 +0100

    feat: notify user/client by SMS when the invoiced is on the way

--- services/shopping/controller.js
+++ services/shopping/controller.js
@@ -6,16 +6,15 @@ const {save, confirmDelivery} = require("./dataccess");
 const commandURL = `http://${process.env.CMD_URL || "localhost:4005/event"}`;
 
 router.post("/", (req, client) => {
-
-    //console.log(req);
-
     const data = ({userId, cartItems, deliveryDate} = req.body);
 
     const handleSaveOrder = ({err, result}) => {
         //console.log(res);
         client.send({result: result.result, obj: result});
         console.log("Passou no primeiro:");
-        sendSMSToClient("Miguel Antonio");
+        
+        //sendOnThewaySMS()
+        
         console.log("Aqui vai mais um");
         
     }
@@ -31,14 +30,15 @@ router.post("/", (req, client) => {
 
 router.put("/order/status", (req, client) => {
 
-    let {id, userId, status} = req.body;
+    let {id, userId, status, userPhone, userName} = req.body;
     id = isNaN(id) == false ? parseInt(id) : id;
 
     //console.log({id, userId, status});
     console.log("*** Command Shopping *** - Dados do alteração: ");
     
     const handleConfirmDelivered = ({err, result}) => {
-        client.send(result);
+        sendOnThewaySMS(userPhone, userName, client, result);
+        //client.send(result);
     }
 
     const handleDeliveredEvent = (res) => {
@@ -64,15 +64,50 @@ const emitDeliveredEvent = (data, callback = (result) => {}) => {
 }
 
 
-const sendSMSToClient = function(clientName = null){
+const sendOnThewaySMS = function(telefone, nomeCompleto, client, result){
 
+    console.log("O numero destino: ",telefone);
+
+    let smsObject = {
+            clientName: telefone,
+            phoneNumber: telefone,
+            onError: (msg) => {
+                console.log(`Conta criada: ${msg}`);
+                client.send({error: true, result: `Houve um erro, tene novamente: ${msg}`});
+            },
+            onSuccess: (msg) => {
+                
+                console.log(`Mensagem de sucesso: ${msg}`);
+                client.send({
+                            error: false, 
+                            result: `O cliente foi ${telefone} foi notificado por SMS que receberá a sua encomenda em breve, por favor, seja responsável`, 
+                            value: ``
+                        });
+            },
+            content: `Caro(a) ${nomeCompleto}, estamos trazendo a sua encomenda`
+    }
+
+    sendSMSToClient(smsObject);
+
+}
+
+
+const sendSMSToClient = function({
+
+    clientName, phoneNumber, onError, onSuccess, content
+
+}){
+
+    console.log(`Conteudo enviado: ${content}`);
+    console.log(`Numero: ${phoneNumber}`);
 
     const https = require('https');
 
     let postData = JSON.stringify({
     'from': 'PPRIMO',
-    'to' : ['+244925927412'],
-    'body': 'Hello World!'
+    'to' : [`+244${phoneNumber}`],
+    'body': content,
+    'encoding': 'UNICODE'
     });
 
     let options = {
@@ -97,6 +132,7 @@ const sendSMSToClient = function(clientName = null){
 
         resp.on('end', () => {
             console.log("Response:", data);
+            onSuccess(data);
         });
 
     });
@@ -104,6 +140,7 @@ const sendSMSToClient = function(clientName = null){
     req.on('error', (e) => {
         console.error("Houve um erro");
         console.error(e);
+        onError(e);
     });
 
     req.write(postData);
