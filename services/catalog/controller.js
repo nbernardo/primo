61af9e0deb8e46ec7ba9e6792aa485ace065f079
Eudes Inacio <eudes.inacio@tistech.co.ao>
2021-01-31 08:27:58 +0100

    feat: Convert from base64 to image file

--- services/catalog/controller.js
+++ services/catalog/controller.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const path = require("path");
+
 const {saveItem, findItems, findById, findItemsByType, update} = require("./dataccess");
 
 router.get("/", (req, resp) => {
@@ -60,29 +62,99 @@ router.get("/item/", (req, client) => {
 })
 
 
+const getImgExtension = (content = "") => {
+
+    const foundType = content.toLowerCase();
+
+    const types = {
+        "data:image/png": "png",
+        "data:image/jpeg": "jpg",
+        "data:image/jpg": "jpg"
+    }
+
+    return types[foundType];
+
+}
+
+const getImgSign = (content = "") => {
+
+    const foundType = content.toLowerCase();
+
+    const types = {
+        "data:image/png": `data:image/png;base64,`,
+        "data:image/jpeg": `data:image/jpeg;base64,`,
+        "data:image/jpg": `data:image/jpg;base64,`
+    }
+
+    return types[foundType];
+
+}
+
+
+const getDirName = (path) => {
+
+    return path.dirname(path);
+
+}
+
+
+const saveImage = (imgContent, nomeImg, callback = () => {}) => {
+
+    let imgType = "";
+    try{
+        imgType = imgContent.toString().split(";base64,")[0];
+    }catch(e){}
+
+    const imageExtension = getImgExtension(imgType);
+    const imagemSignature = getImgSign(imgType);
+
+    const finalImgContent = imgContent.replace(imagemSignature,"");
+    const filePath = path.join(__dirname,"/images/item");
+    
+    require("fs").writeFile(`${filePath}/${nomeImg}.${imageExtension}`,finalImgContent,'base64', (err) => {
+
+        //console.log("Houve um erro", err);
+        //client.send(err);
+        callback(err, `http://siteaddr/${nomeImg}.${imageExtension}`);
+
+    })
+
+}
 
 
 router.post("/item", (req, client) => {
+    
+    const imgContent = req.body.imagem;
+    const nomeImg = req.body.nome;
 
-    saveItem({...req.body}, (res, err) => {
+    const imageSaveCallbck = (res, err) => {
 
         console.log("Passou");
         console.log(res);
 
         if(err){
-            client.status(204).send({
-                    erros: {...err},
-                    result: {...res}
-            });
+            client.status(204).send({erros: {...err},result: {...res}});
             return false;
         }
 
-        client.send({
-            result: res,
-            status: "ok"
-        });
+        client.send({result: res,status: "ok"});
 
-    })
+    }
+
+    const parsistImgOnDb = (err, imagem) => {
+
+        if(err){
+            client.send({erros: {...err}})
+            return false;
+        }
+        saveItem({...req.body, imagem}, imageSaveCallbck);
+
+    }
+
+    saveImage(imgContent, nomeImg, parsistImgOnDb);
+
+    //return;
+
     
 
 })
